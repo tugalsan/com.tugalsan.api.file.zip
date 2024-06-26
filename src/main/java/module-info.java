7370c7f826947c7c8c495a73a488e41c142c6996
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 13:36:35 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,8 @@ module com.tugalsan.api.file.zip {
     requires com.tugalsan.api.file;
     
     requires com.tugalsan.api.callable;
-    requires com.tugalsan.api.validator;
-    requires com.tugalsan.api.coronator;
+    
+    
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.os;
