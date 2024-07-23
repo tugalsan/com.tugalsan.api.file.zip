da9ed2e5ba11fb84ac6bcbff983cfbd199b15976
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-23 15:24:25 +0300

    .

--- src/main/java/com/tugalsan/api/file/zip/server/zip4j/TS_FileZipZip4JUtils.java
+++ src/main/java/com/tugalsan/api/file/zip/server/zip4j/TS_FileZipZip4JUtils.java
@@ -6,6 +6,7 @@ import net.lingala.zip4j.*;
 import com.tugalsan.api.file.server.*;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.list.client.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuseVoid;
 import com.tugalsan.api.unsafe.client.*;
 
 public class TS_FileZipZip4JUtils {
@@ -24,31 +25,34 @@ public class TS_FileZipZip4JUtils {
         zipList(null, TGS_ListUtils.of(sourceDirectory), targetZipFile);
     }
 
-    public static void zipList(List<Path> sourceFiles, List<Path> sourceDirectories, Path targetZipFile) {
-        d.ci("zipList", sourceFiles, sourceDirectories, targetZipFile);
-        TS_DirectoryUtils.createDirectoriesIfNotExists(targetZipFile.getParent());
-        TS_FileUtils.deleteFileIfExists(targetZipFile);
-        var zipFile = new ZipFile(targetZipFile.toAbsolutePath().toString());
-        if (sourceFiles != null) {
-            sourceFiles.stream().forEachOrdered(p -> {
-                TGS_UnSafe.run(() -> {
-                    if (p == null) {
-                        return;
-                    }
-                    zipFile.addFile(p.toFile());
+    public static TGS_UnionExcuseVoid zipList(List<Path> sourceFiles, List<Path> sourceDirectories, Path targetZipFile) {
+        return TGS_UnSafe.call(() -> {
+            d.ci("zipList", sourceFiles, sourceDirectories, targetZipFile);
+            TS_DirectoryUtils.createDirectoriesIfNotExists(targetZipFile.getParent());
+            TS_FileUtils.deleteFileIfExists(targetZipFile);
+            var zipFile = new ZipFile(targetZipFile.toAbsolutePath().toString());
+            if (sourceFiles != null) {
+                sourceFiles.stream().forEachOrdered(p -> {
+                    TGS_UnSafe.run(() -> {
+                        if (p == null) {
+                            return;
+                        }
+                        zipFile.addFile(p.toFile());
+                    });
                 });
-            });
-        }
-        if (sourceDirectories != null) {
-            sourceDirectories.stream().forEachOrdered(p -> {
-                TGS_UnSafe.run(() -> {
-                    if (p == null) {
-                        return;
-                    }
-                    zipFile.addFolder(p.toFile());
+            }
+            if (sourceDirectories != null) {
+                sourceDirectories.stream().forEachOrdered(p -> {
+                    TGS_UnSafe.run(() -> {
+                        if (p == null) {
+                            return;
+                        }
+                        zipFile.addFolder(p.toFile());
+                    });
                 });
-            });
-        }
+            }
+            return TGS_UnionExcuseVoid.ofVoid();
+        }, e -> TGS_UnionExcuseVoid.ofExcuse(e));
     }
 
     public static void unzip(Path sourceZipFile, Path destinationDirectory) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.tugalsan.api.file.zip {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.file;
+    requires com.tugalsan.api.union;
     
     requires com.tugalsan.api.function;
     
